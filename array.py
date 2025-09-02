e14351aaa27f1c306c5606dcfbf7a179fdef0ddc
eudav1d <davidnunes520@gmail.com>
2025-09-01 22:12:16 -0300

    Add files via upload

---
+++ array.py
@@ -0,0 +1,11 @@
+# Crie um array (lista em Python) com 5 nomes de colegas da turma.
+nomes_colegas = ["Roni", "Kayky", "David", "Amanda", "Mauricio"]
+
+print(f"O primeiro elemento é: {nomes_colegas[0]}")
+print(f"O terceiro elemento é: {nomes_colegas[2]}")
+print(f"O último elemento é: {nomes_colegas[-1]}")
+
+if "Carla" in nomes_colegas:
+    print("Carla está na lista!")
+else:
+    print("Carla não foi encontrada na lista.")
